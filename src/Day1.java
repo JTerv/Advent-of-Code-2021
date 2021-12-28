40100c356e787385734447343a5954607e900cbb
Jack Tervay <jacktervay96@gmail.com>
2021-12-28 00:31:24 -0500

    Day 1 complete (actually this time)

--- src/Day1.java
+++ src/Day1.java
@@ -1,6 +1,3 @@
-import java.io.BufferedReader;
-import java.io.FileInputStream;
-import java.io.InputStreamReader;
 import java.nio.charset.Charset;
 import java.nio.file.Files;
 import java.nio.file.Paths;
@@ -13,16 +10,41 @@ public class Day1 {
 
         // Start previousInt as the first number in the txt file
         int previousInt = Integer.parseInt(list.get(0));
-        int countAmount = 0;
+        int part1Count = 0;
+        int part2Count = 0;
+
+        int measureWindow = 3;
+        int rollingCurrTotalValue = 0;
+        int rollingPrevTotalValue = Integer.parseInt(list.get(0) + list.get(1) + list.get(2));
+        //int rollingPrevTotalValue = 0;
 
         for (int i = 0; i < list.size(); i++) {
             if (Integer.parseInt(list.get(i)) > previousInt) {
-                countAmount++;
+                part1Count++;
             }
             previousInt = Integer.parseInt(list.get(i));
+
+            rollingCurrTotalValue = 0;
+            try {
+                for (int j = i; j < i + measureWindow; j++) {
+                    //System.out.println("VALUES: " + list.get(j));
+                    rollingCurrTotalValue = rollingCurrTotalValue + Integer.parseInt(list.get(j));
+                }
+            } catch (Exception e) {
+                rollingCurrTotalValue = 0;
+            }
+
+            System.out.println("TOTAL VALUE: " + rollingCurrTotalValue);
+
+            if (rollingCurrTotalValue > rollingPrevTotalValue) {
+                part2Count++;
+            }
+            rollingPrevTotalValue = rollingCurrTotalValue;
+
         }
 
-        System.out.println(countAmount);
+        System.out.println("Part 1 answer: " + part1Count);
+        System.out.println("Part 2 answer: " + part2Count);
     }
 
 }
